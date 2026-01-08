dcc237493822c6f8aabcad2e8c87572d766e7521
Abhishek_Jain <abhishekjain171103@gmail.com>
2026-01-08 22:40:24 +0530

    .

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/news')
+mongoose.connect('mongodb+srv://abhishekjain011a_db_user:Abhishek123@cluster0.x0dep5m.mongodb.net/news')
 .then(() => console.log('connected to mongodb'))
 
 
